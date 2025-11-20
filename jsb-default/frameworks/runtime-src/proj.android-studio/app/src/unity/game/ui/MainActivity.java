8d3562365941fd53ff6e1aa3c53b25c425973ea8
1126308222@qq.com <1126308222@qq.com>
2025-11-20 19:29:12 +0800

    测试版本

---
+++ jsb-default/frameworks/runtime-src/proj.android-studio/app/src/unity/game/ui/MainActivity.java
@@ -0,0 +1,204 @@
+/****************************************************************************
+Copyright (c) 2015-2016 Chukong Technologies Inc.
+Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+package unity.game.ui;
+
+import org.cocos2dx.javascript.Constants;
+import org.cocos2dx.javascript.SDKManager;
+import org.cocos2dx.javascript.SDKWrapper;
+import org.cocos2dx.javascript.androidSDK.AppsFlyerSDK.AppsFlyerManager;
+import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+
+import java.util.Date;
+
+public class MainActivity extends Cocos2dxActivity {
+
+    public static Context context = null;
+    public static String filePath;
+
+    private void InitConfig(){
+        AppsFlyerManager.AF_DEV_KEY = "BmPiUKNgDqLUAZYP2fKoGd";
+    }
+    // 方法：检查键盘是否显示
+    private boolean isKeyboardVisible() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            return imm.isAcceptingText();  // 如果软键盘显示，返回 true
+        }
+        return false;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.InitConfig();
+
+
+        super.onCreate(savedInstanceState);
+        // Workaround in
+        // https://stackoverflow.com/questions/16283079/re-launch-of-activity-on-home-button-but-only-the-first-time/16447508
+        if (!isTaskRoot()) {
+            // Android launched another instance of the root activity into an existing task
+            // so just quietly finish and go away, dropping the user back into the activity
+            // at the top of the stack (ie: the last state of this task)
+            // Don't need to finish it again since it's finished in super.onCreate .
+            return;
+        }
+        // DO OTHER INITIALIZATION BELOW
+        SDKWrapper.getInstance().init(this);
+
+        context = MainActivity.this;
+
+        Date now = new Date();
+        long time = now.getTime();
+        SDKManager.getSDKManager().InitSDKManager(this,time);
+        //不休眠，需要添加权限WAKE_LOCK
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+
+    @Override
+    public Cocos2dxGLSurfaceView onCreateView() {
+        Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
+        // TestCpp should create stencil buffer
+        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
+        SDKWrapper.getInstance().setGLSurfaceView(glSurfaceView, this);
+
+        return glSurfaceView;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        AdjustManager.getAdjustManager().onActivityResumed();
+        SDKWrapper.getInstance().onResume();
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        AdjustManager.getAdjustManager().onActivityPaused();
+        SDKWrapper.getInstance().onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Workaround in https://stackoverflow.com/questions/16283079/re-launch-of-activity-on-home-button-but-only-the-first-time/16447508
+        if (!isTaskRoot()) {
+            return;
+        }
+
+        SDKWrapper.getInstance().onDestroy();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        SDKWrapper.getInstance().onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        SDKWrapper.getInstance().onNewIntent(intent);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        SDKWrapper.getInstance().onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SDKWrapper.getInstance().onStop();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Constants.CallUnityFunction("",Constants.CallAndroidCallBack);
+        SDKWrapper.getInstance().onBackPressed();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        SDKWrapper.getInstance().onConfigurationChanged(newConfig);
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); //就可以显示出状态栏了
+            // ActionBar 不适用于 Cocos 默认 theme，可略过
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN); //就可以显示出状态栏了
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        SDKWrapper.getInstance().onRestoreInstanceState(savedInstanceState);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        SDKWrapper.getInstance().onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStart() {
+        SDKWrapper.getInstance().onStart();
+        super.onStart();
+    }
+
+    public static MainActivity ctx(){
+        return (MainActivity) SDKWrapper.getInstance().getContext();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+    }
+
+
+}
