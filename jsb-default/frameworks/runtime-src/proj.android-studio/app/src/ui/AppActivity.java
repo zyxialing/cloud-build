cc205ef22e52c609afec74d6366805a081719c70
1126308222@qq.com <1126308222@qq.com>
2025-11-21 11:06:45 +0800

    bug

--- jsb-default/frameworks/runtime-src/proj.android-studio/app/src/ui/AppActivity.java
+++ jsb-default/frameworks/runtime-src/proj.android-studio/app/src/ui/AppActivity.java
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package ui.AppActivity;
+package ui;
 
 import org.cocos2dx.javascript.Constants;
 import org.cocos2dx.javascript.SDKManager;
