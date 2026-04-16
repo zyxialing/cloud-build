8712288bd56f92a96534941c252d60b63e740758
1126308222@qq.com <1126308222@qq.com>
2026-04-16 14:09:55 +0800

    5201002

--- jsb-default/frameworks/runtime-src/proj.android-studio/app/proguard-rules.pro
+++ jsb-default/frameworks/runtime-src/proj.android-studio/app/proguard-rules.pro
@@ -22,7 +22,7 @@
 -dontwarn org.cocos2dx.javascript.SDKConfig
 -dontwarn org.cocos2dx.javascript.SDKManager
 -dontwarn org.cocos2dx.javascript.Constants
--dontwarn ui.BoardAppActivity
+-dontwarn ui.TornadoAppActivity
 
 -keep class com.earn.dev.gamelib.** { *; }
 -dontwarn org.cocos2dx.javascript.SDKConfig
--- jsb-default/frameworks/runtime-src/proj.android-studio/app/src/ui/TornadoAppActivity.java
+++ jsb-default/frameworks/runtime-src/proj.android-studio/app/src/ui/TornadoAppActivity.java
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package ui;
+package ui.TornadoAppActivity;
 
 import org.cocos2dx.javascript.Constants;
 import org.cocos2dx.javascript.SDKConfig;
@@ -56,7 +56,7 @@ import utils.FileEncryptor;
 import utils.MusicPicker;
 import utils.PhotoAgent;
 
-public class BoardAppActivity extends Cocos2dxActivity {
+public class TornadoAppActivity extends Cocos2dxActivity {
 
     public static Context context = null;
     public static String filePath;
@@ -132,7 +132,7 @@ public class BoardAppActivity extends Cocos2dxActivity {
         // DO OTHER INITIALIZATION BELOW
         SDKWrapper.getInstance().init(this);
 
-        context = BoardAppActivity.this;
+        context = TornadoAppActivity.this;
 
         Date now = new Date();
         long time = now.getTime();
@@ -250,8 +250,8 @@ public class BoardAppActivity extends Cocos2dxActivity {
         super.onStart();
     }
 
-    public static BoardAppActivity ctx(){
-        return (BoardAppActivity) SDKWrapper.getInstance().getContext();
+    public static TornadoAppActivity ctx(){
+        return (TornadoAppActivity) SDKWrapper.getInstance().getContext();
     }
 
     @Override
